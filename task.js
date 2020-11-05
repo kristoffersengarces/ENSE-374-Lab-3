35afd9264847a88e7ec6a729275b35797be6a772
Kristoffersen Garces <kristoffersengarces17@outlook.com>
2020-11-04 23:45:10 -0600

    create lab3 files

---
+++ task.js
@@ -0,0 +1,8 @@
+function Task(id, name, owner, creator, done, cleared) {
+    this.id = id;
+    this.name = name;
+    this.owner = owner;
+    this.creator = creator;
+    this.done = done;
+    this.cleared = cleared;
+}
